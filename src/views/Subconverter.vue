0db617a23dadf4fc24c3a578a2a62deef86442d0
zjy7723 <46086433+zjy7723@users.noreply.github.com>
2021-05-30 22:16:28 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "app.zjy7722.ml": "https://app.zjy7722.ml/sub?",
+          "app.zjy7788.xyz": "https://app.zjy7788.xyz/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
